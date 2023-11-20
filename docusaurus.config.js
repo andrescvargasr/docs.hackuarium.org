bb1ba7cabd3a253b87829aa1983befa893cb8e7e
andrescvargasr <andresc.vargasr@gmail.com>
2023-11-20 21:34:55 +0100

    ops: official url from hackuarium

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://andrescvargasr.github.io',
+  url: 'https://docs.hackuarium.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs.hackuarium.org/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'andrescvargasr', // Usually your GitHub org/user name.
+  organizationName: 'hackuarium', // Usually your GitHub org/user name.
   projectName: 'docs.hackuarium.org', // Usually your repo name.
 
   onBrokenLinks: 'throw',
